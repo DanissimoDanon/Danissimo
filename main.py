3ec649dc06f7c2e14c9f370d8225a99803b78154
artemasd <36417885+artemasd@users.noreply.github.com>
2021-03-03 23:59:16 +0300

    UPD

--- main.py
+++ main.py
@@ -5,8 +5,54 @@ class MainWindow(QMainWindow):
         QMainWindow.__init__(self)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        UIFunctions.removeTitleBar(True)
+        self.setWindowTitle('Main Window - Python Base')
+        UIFunctions.labelTitle(self, 'Main Window - Python Base')
+        UIFunctions.labelDescription(self, 'Set text')
+        startSize = QSize(1000, 720)
+        self.resize(startSize)
+        self.setMinimumSize(startSize)
+        self.ui.btn_toggle_menu.clicked.connect(lambda: UIFunctions.toggleMenu(self, 220, True))
 
+        self.ui.stackedWidget.setMinimumWidth(20)
+        UIFunctions.addNewMenu(self, "Главная", "btn_home", "url(:/16x16/icons/16x16/cil-home.png)", True)
+        UIFunctions.addNewMenu(self, "Редактировать профиль", "btn_profile",
+                               "url(:/16x16/icons/16x16/cil-equalizer.png)", False)
+
+        UIFunctions.selectStandardMenu(self, "btn_home")
+        self.ui.stackedWidget.setCurrentWidget(self.ui.page_home)
+
+        def moveWindow(event):
+            if UIFunctions.returStatus() == 1:
+                UIFunctions.maximize_restore(self)
+            if event.buttons() == Qt.LeftButton:
+                self.move(self.pos() + event.globalPos() - self.dragPos)
+                self.dragPos = event.globalPos()
+                event.accept()
+        self.ui.frame_label_top_btns.mouseMoveEvent = moveWindow
+
+        UIFunctions.uiDefinitions(self)
         self.show()
+
+    def Button(self):
+        btnWidget = self.sender()
+        if btnWidget.objectName() == "btn_home":
+            self.ui.stackedWidget.setCurrentWidget(self.ui.page_home)
+            UIFunctions.resetStyle(self, "btn_home")
+            UIFunctions.labelPage(self, "Home")
+            btnWidget.setStyleSheet(UIFunctions.selectMenu(btnWidget.styleSheet()))
+
+        if btnWidget.objectName() == "btn_new_user":
+            self.ui.stackedWidget.setCurrentWidget(self.ui.page_home)
+            UIFunctions.resetStyle(self, "btn_new_user")
+            UIFunctions.labelPage(self, "New User")
+            btnWidget.setStyleSheet(UIFunctions.selectMenu(btnWidget.styleSheet()))
+
+        if btnWidget.objectName() == "btn_profile":
+            self.ui.stackedWidget.setCurrentWidget(self.ui.page_profile)
+            UIFunctions.resetStyle(self, "btn_profile")
+            UIFunctions.labelPage(self, "Custom Widgets")
+            btnWidget.setStyleSheet(UIFunctions.selectMenu(btnWidget.styleSheet()))
 
     def mousePressEvent(self, event):
         self.dragPos = event.globalPos()
