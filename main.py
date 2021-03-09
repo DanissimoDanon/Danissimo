1a7739a64a1d8220385266768385b875e185e32a
artemasd <36417885+artemasd@users.noreply.github.com>
2021-03-09 20:36:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ class MainWindow(QMainWindow):
         ###==UI_START==BEGIN==###
         UIFunctions.labelTitle(self, 'STUDY - Дистанционное обучение')
         self.ui.label_credits.setText('Создано в целях обучения для обучения')
+        self.ui.label_version.setText('DEBUG')
 
         startSize = QSize(1000, 720)
         self.resize(startSize)
@@ -22,6 +23,9 @@ class MainWindow(QMainWindow):
         UIFunctions.labelDescription(self, 'Требуется авторизация')
         self.ui.stackedWidget.setCurrentWidget(self.ui.page_login)
         UIFunctions.labelPage(self, 'Вход')
+
+        UIFunctions.uiDefinitions(self)
+        DataBase.createDb(self)
         ###==UI_START==END==###
 
         ###==CONNECT==BEGIN==###
@@ -52,15 +56,13 @@ class MainWindow(QMainWindow):
                 event.accept()
         self.ui.frame_label_top_btns.mouseMoveEvent = moveWindow
         ###==MOVE_FRAME==END==###
+        self.show()
 
-        UIFunctions.uiDefinitions(self)
-
+        ###==DEBUG==BEGIN==###
         self.ui.edit_login.setText('admin')
         self.ui.edit_pass.setText('admin')
-
-        DataBase.createDb(self)
-        self.show()
         self.ui.btn_login.click()
+        ###==DEBUG==END==###
         # DataBase.insertUser(self, login, self.ui.edit_pass.text(), fname, lname, clas, group, role)
 
     def buttons(self):
@@ -71,7 +73,7 @@ class MainWindow(QMainWindow):
             self.user = cur.fetchone()
             if self.user is not None:
                 self.user = array(self.user)
-                self.ui.stackedWidget.setCurrentWidget(self.ui.page_admin)  # REPAIR
+                self.ui.stackedWidget.setCurrentWidget(self.ui.page_admin)  # ЗАМЕНИТЬ page_admin на page_login
                 UIFunctions.labelPage(self, 'Главная')
                 UIFunctions.labelDescription(self, 'Добро пожаловать, {} {} - {}'.format(
                     self.user[1], self.user[2], self.user[5]))
@@ -124,21 +126,29 @@ class MainWindow(QMainWindow):
             UIFunctions.labelPage(self, 'Вход')
             self.ui.frame_toggle.hide()
             self.ui.frame_left_menu.hide()
+            self.ui.btn_addUsers_page.hide()
 
     def adminBut(self):
         btn = self.sender()
         if btn.objectName() == 'btn_users_addUser' and self.ui.edit_users_name.text():
             try:
+                self.ui.label_users_err.clear()
                 fname = self.ui.edit_users_name.text().split(' ')[0]
                 lname = self.ui.edit_users_name.text().split(' ')[1]
                 login = randGen()
                 passw = randGen()
                 print(self.ui.combo_users_role.itemText(self.ui.combo_users_role.currentIndex()))
+                if self.ui.combo_users_role.currentIndex():
+                    pass
+                else:
+                    self.ui.label_users_err.setText('Имя указано не верно, пример (Иван Иванов).')
                 # DataBase.insertUser(self, login, passw, fname, lname,
                 #                     self.ui.combo_users_role.itemText(self.ui.combo_users_role.currentIndex()),
                 #                     )
             except IndexError:
-                self.ui.label_users_err.setText('Имя указано не верно, пример (Иван Иванов)')
+                self.ui.label_users_err.setText('Имя указано не верно, пример (Иван Иванов).')
+        elif btn.objectName() == 'btn_users_addUser' and not self.ui.edit_users_name.text():
+            self.ui.label_users_err.setText('Имя не может быть пустым.')
 
         if btn.objectName() == 'btn_users_showTable':
             if btn.text() == 'Увеличить таблицу...':
